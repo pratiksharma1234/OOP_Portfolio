7dc9322678c97536530bd0b43d6340bf0509dbe2
pratiksharma <sharmapratikvai4444@gmail.com>
2025-02-21 22:21:42 +0545

    Add files via upload

---
+++ week1/Diamond.java
@@ -0,0 +1,16 @@
+package week1;
+
+public class Diamond {
+
+	public static void main(String[] args) {
+		  System.out.println("   *   ");
+	      System.out.println("  ***  ");
+	      System.out.println(" ***** ");
+	      System.out.println("*******");
+	      System.out.println(" ***** ");
+	      System.out.println("  ***  ");
+	      System.out.println("   *   ");
+
+	}
+
+}
